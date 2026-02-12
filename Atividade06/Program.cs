40aba4e6f5bb6802e47f1c5e33ac545dbe5385bb
ferreirajunior-dev <juniorsenaidev@gmail.com>
2026-02-11 21:53:56 -0300

    Add files via upload

---
+++ Atividade06/Program.cs
@@ -0,0 +1,23 @@
+﻿int opcao;
+double usertemp, convcelsius1, convcelsius2;
+
+Console.Write("Informe a seguir usando somente números, a temperatura em graus celsius que você deseja converter:");
+usertemp = int.Parse(Console.ReadLine());
+
+Console.WriteLine("1 - Fahrenheit");
+Console.WriteLine("2 - Kelvin");
+Console.Write("Informe para qual das unidades de temperatura acima deseja converter: ");
+opcao = int.Parse(Console.ReadLine());
+
+switch (opcao)
+{
+    case 1: // fahrenheit
+    convcelsius1 = (usertemp * 1.8) + 32;
+    Console.WriteLine($"A conversão de {usertemp}°C é: {convcelsius1}°F");
+    break;
+
+    case 2: // kelvin
+    convcelsius2 = usertemp + 273.15;
+    Console.WriteLine($"A conversão de {usertemp}°C é: {convcelsius2}K");
+    break;
+}
