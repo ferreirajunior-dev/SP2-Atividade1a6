78d6309a34ac53d8bf792733f550b030ac303529
ferreirajunior-dev <juniorsenaidev@gmail.com>
2026-02-11 21:46:25 -0300

    Add files via upload

---
+++ Atividade01/Program.cs
@@ -0,0 +1,15 @@
+﻿double num1, num2, num3, resultado, resultado1;
+
+Console.WriteLine("A seguir, informe três números que serão usados para calcular a média aritmética deles:");
+Console.Write("Informe o primeiro número: ");
+num1 = double.Parse(Console.ReadLine());
+
+Console.Write("Informe o segundo número: ");
+num2 = double.Parse(Console.ReadLine());
+
+Console.Write("Informe o terceiro número: ");
+num3 = double.Parse(Console.ReadLine());
+
+resultado = num1 + num2 + num3;
+resultado1 = resultado / 3;
+Console.WriteLine($"A soma dos três números é: {resultado}, e portanto, a média aritmética é: {resultado1}");
---
+++ Atividade03/Program.cs
@@ -0,0 +1,17 @@
+﻿string aluno;
+double nota1, nota2, nota3, mediavar;
+
+Console.WriteLine("Informe o nome do aluno:");
+aluno = Console.ReadLine();
+
+Console.WriteLine("Informe a primeira nota dele:");
+nota1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe a segunda nota dele:");
+nota2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe a terceira nota dele:");
+nota3 = double.Parse(Console.ReadLine());
+
+mediavar = (nota1 + nota2 + nota3) / 3;
+Console.WriteLine($"A média aritmética das notas do {aluno} é: {mediavar}");
